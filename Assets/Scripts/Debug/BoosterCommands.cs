c7d1f51cebca7e088a016a9bd1379c69edad3552
Peymaan <pmnarimani@gmail.com>
2024-06-09 00:13:45 +0200

    debug commands

---
+++ Assets/Scripts/Debug/BoosterCommands.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using IngameDebugConsole;
+using RogueIslands;
+using RogueIslands.Boosters;
+using RogueIslands.View;
+
+namespace Debug
+{
+    public static class BoosterCommands
+    {
+        [ConsoleMethod("add_booster", "Add a booster to the player's inventory. Usage: add_booster <booster_name>")]
+        public static void AddBooster(string name)
+        {
+            var booster = GameManager.Instance.State.AvailableBoosters.FirstOrDefault(CompareName(name));
+            if (booster == null)
+            {
+                UnityEngine.Debug.LogError($"Failed to find a booster with name {name}");
+                return;
+            }
+
+            GameManager.Instance.State.AddBooster(GameManager.Instance, booster);
+            GameManager.Instance.GetUI().RefreshAll();
+        }
+
+        [ConsoleMethod("remove_booster",
+            "Remove a booster from the player's inventory. Usage: remove_booster <booster_name>")]
+        public static void RemoveBooster(string name)
+        {
+            var booster = GameManager.Instance.State.Boosters.FirstOrDefault(CompareName(name));
+            if (booster == null)
+                return;
+            GameManager.Instance.State.RemoveBooster(GameManager.Instance, booster.Id);
+            GameManager.Instance.GetUI().RefreshAll();
+        }
+
+        private static Func<Booster, bool> CompareName(string name)
+        {
+            name = name.Replace(" ", "");
+            return x => x.Name.Replace(" ", "").Equals(name, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ Assets/Scripts/Debug/DateCommands.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using IngameDebugConsole;
+using RogueIslands;
+using RogueIslands.View;
+
+namespace Debug
+{
+    public static class DateCommands
+    {
+        [ConsoleMethod("set_date", "Set the date to the specified month and week. Usage: set_date <month> <week>")]
+        public static void SetDate(int month, int week)
+        {
+            GameManager.Instance.State.Month = month;
+            GameManager.Instance.State.Week = week;
+            GameManager.Instance.GetUI().RefreshAll();
+        }
+        
+        [ConsoleMethod("set_rem_days", "Set the remaining days to the specified value. Usage: set_rem_days <days>")]
+        public static void SetRemainingDays(int remainingDays)
+        {
+            if(remainingDays > GameManager.Instance.State.TotalDays)
+                remainingDays = GameManager.Instance.State.TotalDays;
+            GameManager.Instance.State.Day = GameManager.Instance.State.TotalDays - remainingDays;
+            GameManager.Instance.GetUI().RefreshAll();
+        }
+        
+        [ConsoleMethod("set_energy", "Set the energy to the specified value. Usage: set_energy <energy>")]
+        public static void SetEnergy(int energy)
+        {
+            GameManager.Instance.State.Energy = energy;
+            GameManager.Instance.GetUI().RefreshAll();
+        }
+        
+        [ConsoleMethod("set_money", "Set the money to the specified value. Usage: set_money <money>")]
+        public static void SetMoney(int money)
+        {
+            GameManager.Instance.State.Money = money;
+            GameManager.Instance.GetUI().RefreshAll();
+        }
+    }
+}
