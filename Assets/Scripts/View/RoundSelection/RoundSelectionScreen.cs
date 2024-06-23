f38ec45ba7c613b4e794fdd517350822bec446d8
Peymaan <pmnarimani@gmail.com>
2024-06-23 18:09:23 +0200

    abondon run

--- Assets/Scripts/View/RoundSelection/RoundSelectionScreen.cs
+++ Assets/Scripts/View/RoundSelection/RoundSelectionScreen.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace RogueIslands.View.RoundSelection
 {
@@ -7,10 +11,15 @@ namespace RogueIslands.View.RoundSelection
     {
         [SerializeField] private RoundView _roundPrefab;
         [SerializeField] private Transform _roundParent;
-
+        [SerializeField] private TextMeshProUGUI _title;
+        [SerializeField] private Button _back;
+        
         private void Start()
         {
             var state = GameManager.Instance.State;
+            
+            _title.text = $"Act {state.Act + 1}";
+            _back.onClick.AddListener(() => SceneManager.LoadScene("MainMenu"));
 
             for (var round = 0; round < GameState.RoundsPerAct; round++)
             {
@@ -21,6 +30,11 @@ namespace RogueIslands.View.RoundSelection
                 roundView.ActivatePlayButton(round == state.Round);
                 if (round == state.Round)
                     roundView.SetPlayButtonAction(PlayRound);
+
+                roundView.transform.DOScale(0, 0.3f)
+                    .From()
+                    .SetEase(Ease.OutBack)
+                    .SetDelay(round * 0.05f);
             }
         }
 
