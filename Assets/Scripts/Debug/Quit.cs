9650bb954128c46839b6d2c0d1edb0c4effc9803
Peymaan <pmnarimani@gmail.com>
2024-07-13 15:07:37 +0200

    added quit

---
+++ Assets/Scripts/Debug/Quit.cs
@@ -0,0 +1,14 @@
+﻿using IngameDebugConsole;
+using UnityEngine.SceneManagement;
+
+namespace RogueIslands.Debug
+{
+    public static class Quit
+    {
+        [ConsoleMethod("quit", "Quits the run")]
+        public static void QuitRun()
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
