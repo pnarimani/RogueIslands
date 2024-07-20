947c47362fa3edc7798e11a48e7e50316f1c4061
Peymaan <pmnarimani@gmail.com>
2024-07-20 21:02:11 +0200

    feature: booster selling

--- Assets/Scripts/Gameplay/View/Boosters/BoosterCardView.cs
+++ Assets/Scripts/Gameplay/View/Boosters/BoosterCardView.cs
@@ -1,23 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using DG.Tweening;
+using Flexalon;
 using RogueIslands.DependencyInjection;
 using RogueIslands.Gameplay.Boosters;
-using RogueIslands.Localization;
+using RogueIslands.Gameplay.View.Shop;
 using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace RogueIslands.Gameplay.View.Boosters
 {
     public class BoosterCardView : MonoBehaviour
     {
-        [SerializeField] private TextMeshProUGUI _name, _sellText;
-
+        [SerializeField] private TextMeshProUGUI _name;
+        [SerializeField] private BuySellPanelView _buySellPanel;
+        
         private BoosterCard _booster;
 
         private void Awake()
         {
+            GetComponentInChildren<FlexalonInteractable>().Clicked.AddListener(arg0 =>
+            {
+                if (_buySellPanel.gameObject.activeSelf)
+                {
+                    _buySellPanel.gameObject.SetActive(false);
+                    RemoveCanvas();
+                }
+                else
+                {
+                    _buySellPanel.gameObject.SetActive(true);
+                    // _buySellPanel.transform.DOPunchScale(Vector3.one * 0.2f, 0.5f);
+                    AddCanvas();
+                }
+            });
+        }
+
+        private void RemoveCanvas()
+        {
+            Destroy(GetComponent<GraphicRaycaster>());
+            Destroy(GetComponent<Canvas>());
+        }
+
+        private void AddCanvas()
+        {
+            var source = gameObject.GetComponentInParent<Canvas>();
+            var canvas = gameObject.AddComponent<Canvas>();
+            gameObject.AddComponent<GraphicRaycaster>();
+            canvas.overrideSorting = true;
+            canvas.sortingOrder = source.sortingOrder + 1;
+            canvas.vertexColorAlwaysGammaSpace = source.vertexColorAlwaysGammaSpace;
+            canvas.additionalShaderChannels = source.additionalShaderChannels;
         }
 
         private void OnDestroy()
@@ -30,8 +61,15 @@ namespace RogueIslands.Gameplay.View.Boosters
             _booster = card;
             _name.text = card.Name;
             GetComponent<BoosterView>().Initialize(card);
+            _buySellPanel.ShowSellButton($"Sell ${card.SellPrice}", OnSellClicked);
+            _buySellPanel.HideBuyButton();
         }
-        
+
+        private void OnSellClicked()
+        {
+            StaticResolver.Resolve<BoosterManagement>().SellBooster(_booster.Id);
+        }
+
         private void OnBoosterReordered()
         {
             // var boosterOrder = new List<BoosterCard>();
---
+++ Assets/Scripts/Gameplay/View/Shop/BuySellPanelView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RogueIslands.Gameplay.View.Shop
+{
+    public class BuySellPanelView : MonoBehaviour
+    {
+        [SerializeField] private Button _sell, _buy;
+        [SerializeField] private TextMeshProUGUI _buyText, _sellText;
+
+        public void ShowSellButton(string text, Action clicked)
+        {
+            _sellText.text = text;
+            _sell.onClick.AddListener(() => clicked());
+            _sell.gameObject.SetActive(true);
+        }
+
+        public void HideBuyButton()
+        {
+            _buy.gameObject.SetActive(false);
+        }
+
+        public void HideSellButton()
+        {
+            _sell.gameObject.SetActive(false);
+        }
+        
+        public void ShowBuyButton(string text, Action clicked)
+        {
+            _buyText.text = text;
+            _buy.onClick.AddListener(() => clicked());
+            _buy.gameObject.SetActive(true);
+        }
+    }
+}
